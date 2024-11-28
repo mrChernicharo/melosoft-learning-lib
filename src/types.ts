72c7297a8a7b9a129de895aec43f957e6bcedb4f
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2024-11-28 16:15:11 -0300

    timestamps as number rather than Date

--- src/types.ts
+++ src/types.ts
@@ -13,8 +13,8 @@ export interface Org {
     id: number;
     name: string;
     image_url: string;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
 export interface OrgUser {
     id: number;
@@ -22,7 +22,6 @@ export interface OrgUser {
     user_id: number;
     role: OrgUserRole;
 }
-
 export interface User {
     id: number;
     name: string;
@@ -30,35 +29,32 @@ export interface User {
     image_url: string;
     links: string[]; // homepage, youtube, linkedin, github, twitter etc..
     job_title?: string; // if instructor
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-
 export interface Password {
     id: number;
     user_id: number;
     password: string;
     salt: string;
     image_url: string;
-    type: PasswordType; // if instructor
-    created_at: Date;
-    updated_at: Date;
+    type: PasswordType;
+    created_at: number;
+    updated_at: number;
 }
-
 export interface InstructorRating {
     id: number;
     user_id: number;
     instructor_id: number;
     rating: number;
     comment: string;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
 
 export interface Course {
     id: number;
     org_id: number;
-    instructor_id: number;
     title: string;
     description: string;
     category: CourseCategory; // <-- consider sub-categories in the future
@@ -67,45 +63,45 @@ export interface Course {
     price: number;
     is_published: boolean;
     image_url: string; // URL to the course's thumbnail or cover image
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-
+export interface CourseInstructor {
+    course_id: number;
+    instructor_id: number;
+}
 export interface CourseSection {
     id: number;
     course_id: number;
     title: string;
     description: string;
     order: number;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-
 export interface CourseRating {
     id: number;
     user_id: number;
     course_id: number;
     rating: number;
     comment: string;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-
 export interface CourseAnnouncement {
     id: number;
     instructor_id: number;
     course_id: number;
     comment: string; // markdown
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-
 export interface UserCourseProgress {
     user_id: number;
     course_id: number;
     completed_lessons: string[]; // Array of lesson IDs
-    enrolled_at: Date;
-    last_accessed: Date;
+    enrolled_at: number;
+    last_accessed: number;
 }
 
 export interface Lesson {
@@ -115,46 +111,42 @@ export interface Lesson {
     course_id: number;
     course_section_id: number;
     order: number;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-
 export interface LessonContent {
     id: number;
     lesson_id: number;
     content_type: ContentType;
     content: string | object; // Can be text, video URL, or other JSON object
     order: number;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-// Q&A
 export interface LessonPost {
     id: number;
     lesson_id: number;
     user_id: number;
     title: string;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
-
 export interface LessonComment {
     id: number;
     postId: number;
     user_id: number;
     comment: string;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
     quoted_comment_id?: string;
 }
-
 export interface CommentReactions {
     id: number;
     comment_id: number;
     reaction: Reaction;
     user_id: number;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
 
 export interface Payment {
@@ -164,15 +156,15 @@ export interface Payment {
     amount: number;
     paymentMethod: PaymentMethod;
     status: PaymentStatus;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
 
 export interface Certificate {
     id: number;
     user_id: number;
     course_id: number;
-    issuedAt: Date;
+    issuedAt: number;
     url: string; // URL to the certificate PDF or other format
 }
 
@@ -182,6 +174,6 @@ export interface Notification {
     title: string;
     message: string;
     isRead: boolean;
-    created_at: Date;
-    updated_at: Date;
+    created_at: number;
+    updated_at: number;
 }
